a501d65adfdc43b2a4cfe2dc7bcb16d5457596fb
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-06-22 18:28:46 +0530

    decoupled code

--- index.js
+++ index.js
@@ -9,9 +9,21 @@ const fetchData = async (searchResult) => {
     return [];
   return response.data.Search;
 }
-
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+<label><b>Search for mMvie</b></label>
+<input type="text" />
+  <div class="dropdown ">
+        <div class="dropdown-menu">
+        <div class="dropdown-content results">         
+        </div>
+      </div>
+    </div>
+`;
 
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value);
   movies.map(movie => {
