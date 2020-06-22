81c69d9f92e87eb4dcaea9100b59fb2ff306d4d0
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-06-22 16:49:16 +0530

    Search time

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hi there!');
+const fetchData = async (searchResult) => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '60b4a84b',
+      s: searchResult,
+    }
+  });
+  console.log(response.data);
+}
+let timeoutId;
+const input = document.querySelector('input');
+const onInput = (event) => {
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(() => {
+    fetchData(event.target.value);
+  }, 1000)
+
+}
+input.addEventListener('input', onInput);
