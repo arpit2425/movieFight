d277db4ec68a1a2c3cf536f4000c8628b6e085c1
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-06-23 15:22:11 +0530

    Refactor fetchData

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-const fetchData = async searchTerm => {
-  const response = await axios.get('http://www.omdbapi.com/', {
-    params: {
-      apikey: 'd9835cc5',
-      s: searchTerm
-    }
-  });
 
-  if (response.data.Error) {
-    return [];
-  }
-
-  return response.data.Search;
-};
 
 createAutoComplete({
   root: document.querySelector('.autocomplete'),
@@ -27,7 +14,21 @@ createAutoComplete({
   },
   inputValue(movie) {
     return movie.Title;
-  }
+  },
+  async fetchData(searchTerm) {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: 'd9835cc5',
+        s: searchTerm
+      }
+    });
+
+    if (response.data.Error) {
+      return [];
+    }
+
+    return response.data.Search;
+  },
 });
 
 const onMovieSelect = async movie => {
